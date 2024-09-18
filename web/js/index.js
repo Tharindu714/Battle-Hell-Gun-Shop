29819e57b59e6292db94831b908fa220fc3ff73f
Tharindu714 <>
2024-09-18 13:02:30 +0530

    Clone Products in home page

--- web/js/index.js
+++ web/js/index.js
@@ -41,26 +41,63 @@ async function checkSignIn() {
             console.log("Not Signed In");
         }
 
+        var home_product = document.getElementById("home-product");
+        let home_product_container = document.getElementById("home-product-container");
+        home_product_container.innerHTML = "";
 
-        const productList = json.products;
-        let i = 1;
-        productList.forEach(product => {
-            document.getElementById("homepage-product-title-" + i).innerHTML = product.title;
-            document.getElementById("homepage-product-a-" + i).href = "single-product.html?id=" + product.id;
-            document.getElementById("homepage-img-" + i).src = "product-images/" + product.id + "/image1.png";
-            document.getElementById("homepage-product-price-" + i).innerHTML = new Intl.NumberFormat(
+
+        json.products.forEach(product => {
+            let home_product_clone = home_product.cloneNode(true);
+
+            //update cards
+            home_product_clone.querySelector("#homepage-product-a-1").href = "single-product.html?id=" + product.id;
+            home_product_clone.querySelector("#homepage-img-1").src = "product-images/" + product.id + "/image1.png";
+            home_product_clone.querySelector("#homepage-product-title-1").innerHTML = product.title;
+            home_product_clone.querySelector("#homepage-product-price-1").innerHTML = new Intl.NumberFormat(
                     "en-US",
                     {
                         minimumFractionDigits: 2
                     }
             ).format(product.price);
-            document.getElementById("home-add-to-cart-" + i).addEventListener("click", (e) => {
+            home_product_clone.querySelector("#home-add-to-cart-1")
+                    .addEventListener(
+                            "click",
+                            (e) => {
+                        addToCart(product.id, 1);
+                        e.preventDefault();
+                    });
+
+            home_product_clone.querySelector("#home-add-to-wishlist-1")
+                    .addEventListener(
+                            "click",
+                            (e) => {
+                        addToWishList(product.id);
+                        e.preventDefault();
+                    });
+
+            home_product_container.appendChild(home_product_clone);
+        });
+
+
+        const productList = json.products;
+        let i = 1;
+        productList.forEach(product => {
+            document.getElementById("home-product-title-" + i).innerHTML = product.title;
+            document.getElementById("home-product-a-" + i).href = "single-product.html?id=" + product.id;
+            document.getElementById("home-img-" + i).src = "product-images/" + product.id + "/image1.png";
+            document.getElementById("home-product-price-" + i).innerHTML = new Intl.NumberFormat(
+                    "en-US",
+                    {
+                        minimumFractionDigits: 2
+                    }
+            ).format(product.price);
+            document.getElementById("homepage-add-to-cart-" + i).addEventListener("click", (e) => {
                 addToCart(product.id, 1);
                 e.preventDefault();
             });
 
-//Wishlist Adding
-            document.getElementById("home-add-to-wishlist-" + i).addEventListener("click", (e) => {
+
+            document.getElementById("homepage-add-to-wishlist-" + i).addEventListener("click", (e) => {
                 addToWishList(product.id);
                 e.preventDefault();
             });
